96f9fa23d302b3b207facd73e50f8897a8202a28
Shubidumdu <skaty4870@naver.com>
2021-04-03 22:07:55 +0900

    Test graphQL with fetch

--- pages/query/preload-query.tsx
+++ pages/query/preload-query.tsx
@@ -1,40 +1,19 @@
-import RelayEnvironment from '../../RelayEnvironment';
-import { graphql, QueryRenderer } from 'react-relay';
-import {
-  preloadQuery,
-  preloadQueryResponse,
-} from './__generated__/preloadQuery.graphql';
+import fetchGraphQL from '../../fetchGraphQL';
 
-const Query = graphql`
-  query preloadQuery {
+export default function PreloadedQuery({ data }) {
+  return <div>ID: {data.viewer.login}</div>;
+}
+
+export async function getStaticProps(context) {
+  const { data } = await fetchGraphQL(`query preloadQuery {
     viewer {
       login
     }
-  }
-`;
+  }`);
 
-const renderQuery = ({
-  error,
-  props,
-}: {
-  error: Error;
-  props: preloadQueryResponse;
-}) => {
-  if (error) {
-    return <div>{error.message}</div>;
-  } else if (props) {
-    return <div>id: {props.viewer.login}</div>;
-  }
-  return <div>Loading</div>;
-};
-
-export default function PreloadedQuery() {
-  return (
-    <QueryRenderer<preloadQuery>
-      environment={RelayEnvironment}
-      query={Query}
-      variables={{}}
-      render={renderQuery}
-    />
-  );
+  return {
+    props: {
+      data,
+    },
+  };
 }
